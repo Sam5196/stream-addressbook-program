5ba44a9fd2b5718a0f8f26e5f5040449e4fb6313
Sam5196 <sammedpachore1008@gmail.com>
2022-08-07 13:21:26 +0530

    Added:search Person in a City or State across the multiple AddressBook

--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -28,6 +28,7 @@ public class Contact {
     }
 
     public void setFirstName(String firstName) {
+
         this.firstName = firstName;
     }
 
@@ -36,14 +37,17 @@ public class Contact {
     }
 
     public void setLastName(String lastName) {
+
         this.lastName = lastName;
     }
 
     public String getAddress() {
+
         return address;
     }
 
     public void setAddress(String address) {
+
         this.address = address;
     }
 
@@ -52,38 +56,47 @@ public class Contact {
     }
 
     public void setCity(String city) {
+
         this.city = city;
     }
 
     public String getState() {
+
         return state;
     }
 
     public void setState(String state) {
+
         this.state = state;
     }
 
     public int getZip() {
+
         return zip;
     }
 
     public void setZip(int zip) {
+
         this.zip = zip;
     }
 
     public long getPhoneNumber() {
+
         return phoneNumber;
     }
 
     public void setPhoneNumber(long phoneNumber) {
+
         this.phoneNumber = phoneNumber;
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
